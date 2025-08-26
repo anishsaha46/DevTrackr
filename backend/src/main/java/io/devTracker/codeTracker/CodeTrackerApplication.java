f77b5bdb1e8668a777f68ecaea1b065a94e2f682
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-08-26 07:53:54 +0530

    initial commit

---
+++ backend/src/main/java/io/devTracker/codeTracker/CodeTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.devTracker.codeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/devTracker/codeTracker/CodeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devTracker.codeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
