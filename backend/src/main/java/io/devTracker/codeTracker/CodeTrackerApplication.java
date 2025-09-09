c1fad0b28eccd2023939cec1f7811088b4f4651f
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-09-09 14:01:59 +0530

    feat: enable scheduling in Spring Boot application
    Add @EnableScheduling annotation to allow scheduled tasks execution

--- backend/src/main/java/io/devTracker/codeTracker/CodeTrackerApplication.java
+++ backend/src/main/java/io/devTracker/codeTracker/CodeTrackerApplication.java
@@ -2,8 +2,10 @@ package io.devTracker.codeTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CodeTrackerApplication {
 
 	public static void main(String[] args) {
