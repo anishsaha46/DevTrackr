61c77c7ff21a1958ce47c69bd6aac3cd07e79223
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-09-20 13:38:14 +0530

    refactor(GitHubCacheService): remove unused imports for cleaner code

--- backend/src/main/java/io/devTracker/codeTracker/Service/GitHubCacheService.java
+++ backend/src/main/java/io/devTracker/codeTracker/Service/GitHubCacheService.java
@@ -8,10 +8,10 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
+// import org.springframework.web.client.RestClientException;
 
 import java.util.Collections;
-import java.util.HashMap;
+// import java.util.HashMap;
 import java.util.Map;
 
 /**
